185fcaffd7eb4c9e3e9136b4cefb325100137ec1
vitoriasue <vitoria.imamura@hotmail.com>
2021-06-28 05:09:38 -0300

    final

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace DIO_series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,15 @@
+namespace DIO_series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Musical = 6,
+        Romance = 7,
+        Suspense = 8,
+        Terror = 9
+    }
+}
